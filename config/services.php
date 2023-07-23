aa7fcc56181da0820d78a8d5baa9df128f4aaaec
Anthony cardenas <logicainformatica18@gmail.com>
2023-07-23 01:33:54 -0500

    arreglando autenticacion google

--- config/services.php
+++ config/services.php
@@ -33,6 +33,6 @@ return [
 'google' => [
     'client_id' => env('GOOGLE_CLIENT_ID'),
     'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-    'redirect' => 'http://localhost:8000/home',
+    'redirect' => 'https://sdc-formularios.com/auth/google/calback',
 ],
 ];
