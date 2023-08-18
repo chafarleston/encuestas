bec72e02dd9d983316e482c49e7430b828891aab
Anthony cardenas <logicainformatica18@gmail.com>
2023-08-17 22:08:56 -0500

    mensaje de finalizar encuesta corregida

--- resources/views/survey_client.blade.php
+++ resources/views/survey_client.blade.php
@@ -169,7 +169,7 @@
                         &nbsp;
                         &nbsp;
                     </p>
-                 <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fsdc.research.survey&tabs=timeline&width=500&height=300&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=859285638195545" width="500" height="340" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                 <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fsdc.research.survey&tabs=timeline&width=500&height=350&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=859285638195545" width="500" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
                 </div>
             </div>
 
